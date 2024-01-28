573f4152b563bae4ef731676c5efbafdae283d15
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2024-01-28 19:30:34 -0300

    Seção 3 - Estruturas de Controle
    ​
    Definição de classes e criação/uso de instâncias de objetos

---
+++ ClassesMetodos/ClassesMetodos01/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Classes e Métodos 01 ##\n");
+
+Pessoa p1 = new Pessoa();
+p1.nome = "Maria";
+p1.idade = 25;
+p1.sexo = "feminino";
+
+Pessoa p2 = new Pessoa();
+p2.nome = "Manuel";
+p2.idade = 23;
+p2.sexo = "masculino";
+
+Console.WriteLine($"{p1.nome} tem {p1.idade} anos e é do sexo {p1.sexo}");
+Console.WriteLine($"{p2.nome} tem {p2.idade} anos e é do sexo {p2.sexo}");
+
+Console.WriteLine("\n## Fim do processamento ##\n");
+
+class Pessoa
+{
+    public string? nome;
+    public int idade;
+    public string? sexo;
+}
